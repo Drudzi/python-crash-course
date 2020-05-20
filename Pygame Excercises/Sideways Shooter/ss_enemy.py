506b0d6d5977e6c7cd878c37dad864f463025074
Drudzi <jonathan.r.axelsson@gmail.com>
2020-05-20 15:23:56 +0200

    Added enemy.png to SS-folder-

---
+++ Pygame Excercises/Sideways Shooter/ss_enemy.py
@@ -0,0 +1,19 @@
+from random import randint
+
+import pygame
+from pygame.sprite import Sprite
+
+class Enemy(Sprite):
+    """A class to manage the enemies (targets) in the game."""
+    
+    def __init__(self, game_instance):
+        super().__init__()
+        self.screen = game_instance.screen
+        self.screen_rect = self.screen.get_rect()
+
+        image_filename = ''
+        self.image = pygame.image.load(image_filename)
+        self.rect = self.image.get_rect()
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
