467367601cb3027b3a4dc7805504518a4f6861b8
Drudzi <jonathan.r.axelsson@gmail.com>
2020-05-22 19:18:31 +0200

    Press P to Play

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -117,6 +117,21 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+        
+        elif event.key == pygame.K_p:
+            self.stats.reset_stats()
+            self.stats.game_active = True
+
+            #Get rid of any remaining aliens and bullets:
+            self.aliens.empty()
+            self.bullets.empty()
+            
+            #Create a new fleet and center the ship:
+            self._create_fleet()
+            self.ship.center_ship()
+
+            #Hide the mouse cursor:
+            pygame.mouse.set_visible(False)
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
