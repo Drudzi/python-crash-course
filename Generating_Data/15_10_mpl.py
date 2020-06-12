f7a11712f4976f668853859f9cd9e0c01cc6525e
Drudzi <jonathan.r.axelsson@gmail.com>
2020-06-12 16:41:25 +0200

    Update 15_10_mpl.py

--- Generating_Data/15_10_mpl.py
+++ Generating_Data/15_10_mpl.py
@@ -14,6 +14,11 @@ results = [die_1.roll() + die_2.roll() for roll in range(10_000)]
 # Analyze results:
 max_result = die_1.roll() + die_2.roll()
 frequencies = [results.count(value) for value in range(2, max_result+1)]
+x_values = list(range(2, max_result+1))
 
 #Visualize results:
-x_values = list(range(2, max_result+1))
+plt.style.use('classic')
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.hist(x=x_values, bins=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], data=frequencies)
+
+plt.show()
