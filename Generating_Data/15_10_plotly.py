9b9ae0d6b710caee25a52e47ef8108042b6049bf
Drudzi <jonathan.r.axelsson@gmail.com>
2020-06-14 11:08:12 +0200

    15_10_plotly finished

--- Generating_Data/15_10_plotly.py
+++ Generating_Data/15_10_plotly.py
@@ -1,7 +1,15 @@
 import plotly.graph_objects as go
+from plotly import offline
 import numpy as np
 
 from random_walk import RandomWalk
 
 rw = RandomWalk(10_000)
 rw.fill_walk()
+
+steps = go.Scatter(x=rw.x_values, y=rw.y_values, mode='markers', 
+    marker=dict(size=4, color=list(range(rw.num_points)), colorscale='Inferno_r', showscale=True),
+    name='Random Walk')
+my_layout = go.Layout(title='Random Walk of 10.000 points')
+
+offline.plot({'data': steps, 'layout': my_layout}, filename='random_walk.html')
