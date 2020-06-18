faba3c14e720f1461ccdbc7843cd872bf489d1f2
Drudzi <jonathan.r.axelsson@gmail.com>
2020-06-18 13:48:03 +0200

    16-9 complete

--- Downloading_Data/16_9.py
+++ Downloading_Data/16_9.py
@@ -3,7 +3,7 @@ import csv
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-filename = 'world_fires_7_day.csv'
+filename = 'data/world_fires_1_day.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header = next(reader)
@@ -17,12 +17,17 @@ with open(filename) as f:
 # Map the fires:
 data = [{
     'type': "scattergeo",
-    'lat': lats,
     'lon': lons,
+    'lat': lats,
     'marker': {
-        'size': [5*bright in brights],
+        'size': 2,
         'color': brights,
-        'colorscale': "Inferno"
-        'colorbar': {'title': "Brightness"}
+        'colorscale': "Inferno",
+        'colorbar': {'title': "Brightness"},
     }
 }]
+
+layout = Layout(title="Global Fires, last 7 days")
+
+fig = {'data': data, 'layout': layout}
+offline.plot(fig, filename='global_fires.html')
