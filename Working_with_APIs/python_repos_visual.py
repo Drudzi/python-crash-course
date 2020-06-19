8b25f65b92d24309c30c31ef87f9d2d89d06d4bf
Drudzi <jonathan.r.axelsson@gmail.com>
2020-06-19 11:04:26 +0200

    Next is clickable links

--- Working_with_APIs/python_repos_visual.py
+++ Working_with_APIs/python_repos_visual.py
@@ -12,16 +12,26 @@ print(f"Status code: {r.status_code}")
 # Process results:
 response_dict = r.json()
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_names, stars, labels = [], [], []
 for repo_dict in repo_dicts: #Collecting data from the API.
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login'] #Name of the owner.
+    description = repo_dict['description'] #Description of the project.
+    label = f"{owner}<br />{description}"
+    #We merge the owner and description together in a string and store it in a variable.
+    #Plotly let's us use HTML code within text elements...
+    # so we generate a line break using (<br />).
+
+    labels.append(label)
 
 # Make visualization:
 data = [{
     'type': 'bar',
     'x': repo_names, #Names of the projects.
     'y': stars, #Amount of stars.
+    'hovertext': labels, #Shows our labels strings when bars are hovered.
     'marker': {
         'color': 'rgb(60, 100, 150)',
         'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
