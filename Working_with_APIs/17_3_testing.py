94d90bb40c0c1cae451a1043997a010ca6ba96c0
Drudzi <jonathan.r.axelsson@gmail.com>
2020-06-25 00:09:25 +0200

    Nästa Django

--- Working_with_APIs/17_3_testing.py
+++ Working_with_APIs/17_3_testing.py
@@ -1,1 +1,15 @@
-from python_repos_visual import status_code
+import unittest
+
+from python_repos_visual import r, repo_dicts
+
+class TestCase(unittest.TestCase):
+    """Tests for the Python Repos Visual."""
+
+    def test_status_code(self):
+        self.assertEqual(r.status_code, 200)
+
+    def test_repo_amount(self):
+        self.assertGreater(len(repo_dicts), 25)
+
+if __name__ == '__main__':
+    unittest.main()
