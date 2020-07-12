e0553fdb0f36d78dbd6e7b1155035af601f00174
Drudzi <jonathan.r.axelsson@gmail.com>
2020-07-12 14:31:04 +0200

    18-8 finished

--- Web_Apps/pizzeria/pizzas/urls.py
+++ Web_Apps/pizzeria/pizzas/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'pizzas'
 urlpatterns = [
     # Home page:
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    # Menu page:
+    path('menu/', views.menu, name='menu'),
+    # Detailed page about each pizza:
+    path('menu/<int:pizza_id>/', views.pizza, name='pizza')
 ]
--- Web_Apps/pizzeria/pizzas/views.py
+++ Web_Apps/pizzeria/pizzas/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from .models import Pizza, Topping
+
 # Create your views here.
 def home(request):
     """The home page."""
     return render(request, 'pizzas/home.html')
+
+def menu(request):
+    """The menu page."""
+    pizzas = Pizza.objects.order_by('name')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/menu.html', context)
+
+def pizza(request, pizza_id):
+    """The detailed page about each pizza on the menu."""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('name')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas/pizza.html', context)
