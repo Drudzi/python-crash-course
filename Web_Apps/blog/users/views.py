3662c3affbbabc3f5f3282e046b55f3f23a6e44f
Drudzi <jonathan.r.axelsson@gmail.com>
2020-07-31 15:35:14 +0200

    19-2 finished

--- Web_Apps/blog/users/views.py
+++ Web_Apps/blog/users/views.py
@@ -4,5 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     """Page for registration of a user."""
-    context = {}
+    if request.method != 'POST':
+        # No POST data submitted, create a blank form:
+        form = UserCreationForm()
+    else:
+        # POST data submitted, process the completed form:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            
+            # Login user, and redirect to the home page:
+            login(request, new_user)
+            return redirect('blogs:home')
+
+    context = {'form': form}
     return render(request, 'registration/register.html', context)
